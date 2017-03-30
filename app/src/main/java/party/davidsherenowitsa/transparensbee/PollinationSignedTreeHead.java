14061748ca9460af1a392f6c5a573e2fd836af17
David Cook <divergentdave@gmail.com>
2017-03-29 20:40:26 -0500

    Boilerplate

--- app/src/main/java/party/davidsherenowitsa/transparensbee/PollinationSignedTreeHead.java
+++ app/src/main/java/party/davidsherenowitsa/transparensbee/PollinationSignedTreeHead.java
@@ -1,5 +1,16 @@
 package party.davidsherenowitsa.transparensbee;
 
 public class PollinationSignedTreeHead extends SignedTreeHead {
-    private byte[] logID;
+    private final byte[] logID;
+
+    public PollinationSignedTreeHead(int version, int signatureType, long timestamp, long treeSize, byte[] rootHash, byte[] logID)
+    {
+        super(version, signatureType, timestamp, treeSize, rootHash);
+        this.logID = logID;
+    }
+
+    public byte[] getLogID()
+    {
+        return logID;
+    }
 }
--- app/src/main/java/party/davidsherenowitsa/transparensbee/SignedTreeHead.java
+++ app/src/main/java/party/davidsherenowitsa/transparensbee/SignedTreeHead.java
@@ -1,9 +1,43 @@
 package party.davidsherenowitsa.transparensbee;
 
 public class SignedTreeHead {
-    private int version;
-    private int signatureType;
-    private long timestamp;
-    private long treeSize;
-    private byte[] rootHash;
+    private final int version;
+    private final int signatureType;
+    private final long timestamp;
+    private final long treeSize;
+    private final byte[] rootHash;
+
+    public SignedTreeHead(int version, int signatureType, long timestamp, long treeSize, byte[] rootHash)
+    {
+        this.version = version;
+        this.signatureType = signatureType;
+        this.timestamp = timestamp;
+        this.treeSize = treeSize;
+        this.rootHash = rootHash;
+    }
+
+    public int getVersion()
+    {
+        return version;
+    }
+
+    public int getSignatureType()
+    {
+        return signatureType;
+    }
+
+    public long getTimestamp()
+    {
+        return timestamp;
+    }
+
+    public long getTreeSize()
+    {
+        return treeSize;
+    }
+
+    public byte[] getRootHash()
+    {
+        return rootHash;
+    }
 }
