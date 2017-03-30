8ed73346fc37aeca1ab0c6a4aa7858ca2b05523c
David Cook <divergentdave@gmail.com>
2017-03-29 19:58:14 -0500

    Set up button for manual testing

--- app/src/main/java/party/davidsherenowitsa/transparensbee/MainActivity.java
+++ app/src/main/java/party/davidsherenowitsa/transparensbee/MainActivity.java
@@ -2,12 +2,27 @@ package party.davidsherenowitsa.transparensbee;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button runButton = (Button)findViewById(R.id.runButton);
+        if (runButton != null) runButton.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId())
+        {
+            case R.id.runButton:
+                System.out.println("Hello world!");
+                break;
+        }
     }
 }
