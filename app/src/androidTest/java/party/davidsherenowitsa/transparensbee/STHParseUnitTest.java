77f038224f6e89055fa78a7129e430858ac9f71d
David Cook <divergentdave@gmail.com>
2017-03-30 23:48:29 -0500

    Expected values first in assertions

--- app/src/androidTest/java/party/davidsherenowitsa/transparensbee/STHParseUnitTest.java
+++ app/src/androidTest/java/party/davidsherenowitsa/transparensbee/STHParseUnitTest.java
@@ -15,11 +15,11 @@ public class STHParseUnitTest {
     {
         String fixture = "{\"tree_size\":46466472,\"timestamp\":1480512258330,\"sha256_root_hash\":\"LcGcZRsm+LGYmrlyC5LXhV1T6OD8iH5dNlb0sEJl9bA=\",\"tree_head_signature\":\"BAMASDBGAiEA/M0Nvt77aNe+9eYbKsv6rRpTzFTKa5CGqb56ea4hnt8CIQCJDE7pL6xgAewMd5i3G1lrBWgFooT2kd3+zliEz5Rw8w==\"}";
         SignedTreeHead sth = LogClient.parseSTH(fixture);
-        assertEquals(sth.getVersion(), 0);
-        assertEquals(sth.getSignatureType(), 1);
-        assertEquals(sth.getTimestamp(), 1480512258330L);
-        assertEquals(sth.getTreeSize(), 46466472L);
-        assertEquals(Base64.encodeToString(sth.getRootHash(), Base64.NO_WRAP), "LcGcZRsm+LGYmrlyC5LXhV1T6OD8iH5dNlb0sEJl9bA=");
-        assertEquals(Base64.encodeToString(sth.getTreeHeadSignature(), Base64.NO_WRAP), "BAMASDBGAiEA/M0Nvt77aNe+9eYbKsv6rRpTzFTKa5CGqb56ea4hnt8CIQCJDE7pL6xgAewMd5i3G1lrBWgFooT2kd3+zliEz5Rw8w==");
+        assertEquals(0, sth.getVersion());
+        assertEquals(1, sth.getSignatureType());
+        assertEquals(1480512258330L, sth.getTimestamp());
+        assertEquals(46466472L, sth.getTreeSize());
+        assertEquals("LcGcZRsm+LGYmrlyC5LXhV1T6OD8iH5dNlb0sEJl9bA=", Base64.encodeToString(sth.getRootHash(), Base64.NO_WRAP));
+        assertEquals("BAMASDBGAiEA/M0Nvt77aNe+9eYbKsv6rRpTzFTKa5CGqb56ea4hnt8CIQCJDE7pL6xgAewMd5i3G1lrBWgFooT2kd3+zliEz5Rw8w==", Base64.encodeToString(sth.getTreeHeadSignature(), Base64.NO_WRAP));
     }
 }
